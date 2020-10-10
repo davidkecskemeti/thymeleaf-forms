fe5d11312c9c746a8aa2800153fe9baa8ae25df5
Dávid Kecskeméti <dxdy421@gmail.com>
2020-10-10 11:51:40 +0200

    Initial commit

---
+++ src/main/java/com/dk/thymeleaf/thymeleafforms/ThymeleafFormsApplication.java
@@ -0,0 +1,13 @@
+package com.dk.thymeleaf.thymeleafforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafFormsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafFormsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dk/thymeleaf/thymeleafforms/ThymeleafFormsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dk.thymeleaf.thymeleafforms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafFormsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
